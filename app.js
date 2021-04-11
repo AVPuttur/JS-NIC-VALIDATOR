f2575adb3c099dc07ae14b266b812a085db2dcd3
AVPuttur <avputtur@gmail.com>
2021-04-11 18:07:18 +0400

    First  commit :) define classes + get value function + trim value

---
+++ app.js
@@ -0,0 +1,29 @@
+//P121196108506G
+//S0808739500254
+let btn = document.getElementsByClassName("validate-nic-mru")[0];
+btn.onclick = validateNICMRU;
+
+function validateNICMRU() {
+    let fname = document.getElementsByClassName('validate-nic-fname')[0].value;
+    let lname = document.getElementsByClassName('validate-nic-lname')[0].value;
+    let dob = document.getElementsByClassName('validate-nic-dob')[0].value;
+
+    console.log(fname+lname+dob);
+    getFirstCharacter(lname);
+}
+
+function getFirstCharacter(lname) {
+	// body...
+	try {
+	  let trimCharacter = lname.replace(/\s+/g, '');
+	  let firstCharater = trimCharacter.charAt(0).toUpperCase();
+	  if(firstCharater.match(/\d+/g)) throw "Numeric value not allowed.";
+	  if(firstCharater == "") throw "Cannot be null."	
+	return firstCharater;  	
+	}
+	catch(err) {
+	  console.error(err);
+	}
+
+
+}
