13bf10bb45883d04744ee2887f96b700d7347f23
AVPuttur <avputtur@gmail.com>
2021-04-18 12:44:25 +0400

    try catch function update

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
 //P141496148506G
 //S0808739500254
-let btn = document.getElementsByClassName("validate-nic-mru")[0];
-btn.onclick = validateNICMRU;
+ let btn = document.getElementsByClassName("validate-nic-mru")[0];
+ btn.onclick = validateNICMRU;
+
 
 function validateNICMRU() {
     let fname = document.getElementsByClassName('validate-nic-fname')[0].value;
     let lname = document.getElementsByClassName('validate-nic-lname')[0].value;
     let dob = document.getElementsByClassName('validate-nic-dob')[0].value;
 
-    console.log(fname+lname+dob);
-    getFirstCharacter(lname);
+    console.log(fname + lname + dob);
+    try {
+        // statements
+        if (getFirstCharacter(lname).match(/\d+/g)) throw "Numeric value not allowed.";
+        if (getFirstCharacter(lname) == "") throw "Cannot be null.";
+
+        console.log(getFirstCharacter(lname));
+    } catch (err) {
+        // statements
+        console.error(err);
+    }
 }
 
 function getFirstCharacter(lname) {
-	// body...
-	try {
-	  let trimCharacter = lname.replace(/\s+/g, '');
-	  let firstCharater = trimCharacter.charAt(0).toUpperCase();
-	  if(firstCharater.match(/\d+/g)) throw "Numeric value not allowed.";
-	  if(firstCharater == "") throw "Cannot be null."	
-	return firstCharater;  	
-	}
-	catch(err) {
-	  console.error(err);
-	}
+    // body...
+    let trimCharacter = lname.replace(/\s+/g, '');
+    let firstCharater = trimCharacter.charAt(0).toUpperCase();
 
-
+    return firstCharater;
 }
