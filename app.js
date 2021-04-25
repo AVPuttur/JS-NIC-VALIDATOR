5a161f4d8dc5fbe5e84849f92ecc5160fe55c1a5
AVPuttur <avputtur@gmail.com>
2021-04-25 13:40:00 +0400

    Its a wrap :) validation added + regex updated + new function added

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
-//P141496148506G
-//S0808739500254
- let btn = document.getElementsByClassName("validate-nic-mru")[0];
- btn.onclick = validateNICMRU;
- 
+let btn = document.getElementsByClassName("validate-nic-mru")[0];
+btn.onclick = validateNICMRU;
+
 function validateNICMRU() {
-    let fname = document.getElementsByClassName('validate-nic-fname')[0].value;
+    //Get value from input
     let lname = document.getElementsByClassName('validate-nic-lname')[0].value;
     let dob = document.getElementsByClassName('validate-nic-dob')[0].value;
-
-    console.log(getDateOfBirth(dob));
+    let nic = document.getElementsByClassName('validate-nic-no')[0].value;
+    //Check for correct data input
     try {
         // statements
         if (getFirstCharacter(lname).match(/\d+/g)) throw "Numeric value not allowed.";
         if (getFirstCharacter(lname) == "") throw "Cannot be null.";
 
-        let x = "S1112963950254";
-        let c = "S";
-		 let regex = /^[\b(c)\b]111296\d{6}[A-Z0-9]$/g;
-		 if (regex.test(x)) {
-		    console.log("Valid");
-		} else {
-		    console.log("Invalid");
-		}
-
-        console.log();
     } catch (err) {
         // statements
         console.error(err);
+    }
+    //Check if the first character of the surname and the DOB match the one in NIC
+    if ((getFirstCharacter(lname) == getFirstCharacter(nic)) && ((nic.slice(1, 7)) == (getDateOfBirth(dob)))) {
+        regexExp(getFirstCharacter(nic) + getDateOfBirth(dob), nic);
+    } else {
+        alert("First character in surname or Date of Birth does not match in NIC.");
+        document.getElementsByClassName("validate-nic-no")[0].style.borderColor = "red";
     }
 }
 
@@ -39,11 +34,22 @@ function getFirstCharacter(value) {
 }
 
 function getDateOfBirth(value) {
-	// body... 
-	let splitText = value.split('-');
-	let year = splitText[0].substring(2);
-	let day = splitText[2];
-	let month = splitText[1];
+    // body... 
+    let splitText = value.split('-');
+    let year = splitText[0].substring(2);
+    let day = splitText[2];
+    let month = splitText[1];
 
-return day+month+year;
+    return day + month + year;
+}
+
+function regexExp(value, nic) {
+    let regex = new RegExp('^' + value + "\\d{6}[A-Z0-9]$");
+    if (regex.test(nic)) {
+        document.getElementsByClassName("validate-nic-no")[0].style.borderColor = "";
+    } else {
+        alert("NIC is not valid. Check again.");
+        document.getElementsByCla
+        ssName("validate-nic-no")[0].style.borderColor = "red";
+    }
 }
