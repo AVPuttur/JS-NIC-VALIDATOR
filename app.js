9c6d366ba9f9d7cc3fad9196bcdda0ad429051e8
AVPuttur <avputtur@gmail.com>
2021-04-30 00:51:49 +0400

    app.js updated

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-function validateNICMRU(option) {
-	if(option.validate_NIC == true){
-	    //Get value from input
-	    let lname = document.getElementsByClassName('validate-nic-lname')[0].value;
-	    let dob = document.getElementsByClassName('validate-nic-dob')[0].value;
-	    let nic = document.getElementsByClassName('validate-nic-no')[0].value;
-	    //Check for correct data input
-	    try {
-	        // statements
-	        if (getFirstCharacter(lname).match(/\d+/g)) throw "Numeric value not allowed.";
-	        if (getFirstCharacter(lname) == "") throw "Cannot be null.";
+let btn = document.getElementsByClassName("validate-nic-mru")[0];
+btn.onclick = validateNICMRU;
 
-	    } catch (err) {
-	        // statements
-	        console.error(err);
-	    }
-	    //Check if the first character of the surname and the DOB match the one in NIC
-	    if ((getFirstCharacter(lname) == getFirstCharacter(nic)) && ((nic.slice(1, 7)) == (getDateOfBirth(dob)))) {
-	        regexExp(getFirstCharacter(nic) + getDateOfBirth(dob), nic);
-	    } else {
-	        alert("First character in surname or Date of Birth does not match in NIC.");
-	        document.getElementsByClassName("validate-nic-no")[0].style.borderColor = "red";
-	    }
+function validateNICMRU() {
+    //Get value from input
+    let lname = document.getElementsByClassName('validate-nic-lname')[0].value;
+    let dob = document.getElementsByClassName('validate-nic-dob')[0].value;
+    let nic = document.getElementsByClassName('validate-nic-no')[0].value;
+    //Check for correct data input
+    try {
+        // statements
+        if (getFirstCharacter(lname).match(/\d+/g)) throw "Numeric value not allowed.";
+        if (getFirstCharacter(lname) == "") throw "Cannot be null.";
+
+    } catch (err) {
+        // statements
+        console.error(err);
+    }
+    //Check if the first character of the surname and the DOB match the one in NIC
+    if ((getFirstCharacter(lname) == getFirstCharacter(nic)) && ((nic.slice(1, 7)) == (getDateOfBirth(dob)))) {
+        regexExp(getFirstCharacter(nic) + getDateOfBirth(dob), nic);
+    } else {
+        alert("First character in surname or Date of Birth does not match in NIC.");
+        document.getElementsByClassName("validate-nic-no")[0].style.borderColor = "red";
     }
 }
 
